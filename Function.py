8413f1a810cc6d62bd5530b47d522348f8b8aea1
Md Hafizur Rahman <hafizur15-1979@diu.edu.bd>
2019-03-12 19:59:31 +0600

    add function

---
+++ Function.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    c=a+b
+    return c
+a,b=5,10
+d=a+b
+print (d)
+
+print ()
+
+def beautiful(text):
+    return text + '!!!'
+
+def make_line(function , word):
+    return 'hellow '+function(word)
+
+print (make_line(beautiful,'word '))
